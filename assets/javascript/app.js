2d6638a1333d97a54f0bb5571e22c2d810fc21f7
Tim Biehl <tbiehl22@gmail.com>
2020-01-24 18:59:45 -0700

    added more javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,31 @@ var gamePlay = {
     startTimer: function()  {
         $("#timer").text("Time Remaining: " + gamePlay.remainingTime);
         setInterval(gamePlay.countdown, 1000);
-           
-        },
+        $("#startPage").hide();
+        triva.displayQuestions();   
+    },
+    
+    countdown: function()   {
+        gamePlay.remainingTime--;
+        $("#timer").text("Time Remaining: " + gamePlay.remainingTime);
+        if (gamePlay.remainingTime === 0)   {
+            gamePlay.stopTimer();
+            $("#timer").empty();
+        }
+    },
+
+    stopTimer: function()    {
+        clearInterval();
+        trivia.checkAnswers();
+    },
+
+    showEndPage: function(correctAnswers, incorrectAnswers, unansweredQuestions)    {
+        $("#endPage").show();
+        $("#triviaQUestions").empty();
+        $("#timer").empty();
+        $("#timer").hide();
+        $("#correct").text("Correct Answers: " + correctAnswers);
+        $("#incorrect").text("Incorrect Answers: " + incorrectAnswers);
+        ("#unanswered").text("Unanswered Questions: " + unansweredQuestions);
     }
 }
